766d900bf97b98d3c3877f2123911244d1491cbb
galawangjudz <jaevoli@gmail.com>
2023-10-23 14:53:07 +0800

    database

---
+++ database/db_utility.sql
@@ -0,0 +1,142 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 09, 2023 at 03:02 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_utility`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', '2020-11-03 05:55:30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbldepartments`
+--
+
+CREATE TABLE `tbldepartments` (
+  `id` int(11) NOT NULL,
+  `DepartmentName` varchar(150) DEFAULT NULL,
+  `DepartmentShortName` varchar(100) NOT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbldepartments`
+--
+
+INSERT INTO `tbldepartments` (`id`, `DepartmentName`, `DepartmentShortName`, `CreationDate`) VALUES
+(2, 'Information Technologies', 'ICT', '2017-11-01 07:19:37'),
+(3, 'Library', 'LIb', '2021-05-21 08:27:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblemployees`
+--
+
+CREATE TABLE `tblemployees` (
+  `emp_id` int(11) NOT NULL,
+  `FirstName` varchar(150) NOT NULL,
+  `LastName` varchar(150) NOT NULL,
+  `EmailId` varchar(200) NOT NULL,
+  `Password` varchar(180) NOT NULL,
+  `Gender` varchar(100) NOT NULL,
+  `Department` varchar(255) NOT NULL,
+  `Phonenumber` char(11) NOT NULL,
+  `Status` int(1) NOT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `role` varchar(30) NOT NULL,
+  `location` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblemployees`
+--
+
+INSERT INTO `tblemployees` (`emp_id`, `FirstName`, `LastName`, `EmailId`, `Password`, `Gender`, `Department`, `Phonenumber`, `Status`, `RegDate`, `role`, `location`) VALUES
+(1, 'Roseann', 'Capule', 'rosea@gmail.com', '0192023a7bbd73250516f069df18b500', 'Female', 'ICT', '0248865955', 1, '2017-11-10 11:29:59', 'Staff', 'NO-IMAGE-AVAILABLE.jpg'),
+(2, 'Jude', 'Dela Cruz', 'jude@gmail.com', '0192023a7bbd73250516f069df18b500', 'Male', 'ICT', '09561305511', 1, '2017-11-10 13:40:02', 'Admin', 'avatar-1.png'),
+(5, 'Elena', 'Millo', 'elena@gmail.com', '0192023a7bbd73250516f069df18b500', 'Female', 'ICT', '587944255', 1, '2017-11-10 13:40:02', 'Cashier', 'photo5.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbldepartments`
+--
+ALTER TABLE `tbldepartments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  ADD PRIMARY KEY (`emp_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbldepartments`
+--
+ALTER TABLE `tbldepartments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
