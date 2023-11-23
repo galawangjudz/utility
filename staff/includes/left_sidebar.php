edc06485f97e27d0eab7cbeb69443d51f022a72f
galawangjudz <jaevoli@gmail.com>
2023-11-23 16:55:06 +0800

    car log

--- staff/includes/left_sidebar.php
+++ staff/includes/left_sidebar.php
@@ -22,16 +22,18 @@
 							<span class="micon dw dw-calendar1"></span><span class="mtext">Accounts</span>
 						</a>
 					</li>
-					<li>
+			<!-- 		<li>
 						<a href="<?php echo base_url ?>staff/?page=get-soa" class="dropdown-toggle no-arrow">
 							<span class="micon dw dw-calendar1"></span><span class="mtext">Bill Report</span>
 						</a>
-					</li>
+					</li> -->
+					<?php if ($_SESSION['user_type'] == 'Cashier'): ?>
 					<li>
 						<a href="<?php echo base_url ?>staff/?page=report/car_logs" class="dropdown-toggle no-arrow">
 							<span class="micon dw dw-newspaper"></span><span class="mtext">CAR Report</span>
 						</a>
 					</li>
+					<?php endif; ?>
 					<li>
 						<div class="dropdown-divider"></div>
 					</li>
