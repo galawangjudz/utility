2f11f0da97e551c3a492674303c53f46c4389108
galawangjudz <jaevoli@gmail.com>
2023-12-13 09:55:34 +0800

    remove pre-loader

--- staff/index.php
+++ staff/index.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html lang="en" class="" style="height: auto;">
 <body>
-	<div class="pre-loader">
+<!-- 	<div class="pre-loader">
 		<div class="pre-loader-box">
 			<div class="loader-logo"><img src="../vendors/images/deskapp-logo-svg.png" alt=""></div>
 			<div class='loader-progress' id="progress_div">
@@ -16,7 +16,7 @@
 				Loading...
 			</div>
 		</div>
-	</div>
+	</div> -->
 
 	<?php include('includes/navbar.php')?>
 
