e537bf8d181adf9fb03c60c6b0dd8a03fa01bc2e
galawangjudz <jaevoli@gmail.com>
2023-12-29 10:08:59 +0800

    soa for staff

--- admin/get-soa/index.php
+++ admin/get-soa/index.php
@@ -21,52 +21,7 @@
                         </div>
                     </form>
                    
-                    <form action="" id="filter">
-                        <div class="row align-items-end">
-                            <input type="hidden" id="page" name="page" value="accounts" class="form-control form-control-sm rounded-0">
-                        
-                       
-                            <div class="col-md-2 form-group">
-                                
-                                <label for="phase" class="control-label">Phase</label>
-                                <select name="phase" id="phase" class="custom-select form-control" autocomplete="off">
-                                  <?php
-                                
-                                    $sql = "SELECT * FROM t_projects ORDER BY c_acronym";
-                                    $results = odbc_exec($conn2, $sql);
-
-
-                                    $selectedValue = isset($_GET['phase']) ? $_GET['phase'] : ''; // Get the selected value from the submitted form
-
-                                    
-                                   
-                                    echo '<option value="" selected>--SELECT--</option>';
-                                    echo '<option value="100">ALL</option>';
-                                    while ($row = odbc_fetch_array($results)) {
-                                        $optionValue = $row['c_code'];
-                                        $optionText = $row['c_acronym'];
-                                        $selected = ($selectedValue == $optionValue) ? 'selected' : ''; // Check if this option is selected
-                                        echo '<option value="' . $optionValue . '" ' . $selected . '>' . $optionText . '</option>';
-                                    }
-                                    echo '</select>';
-                                        
-                                    
-                                ?>
-                            </div>
-                            <div class="col-md-2 form-group">
-                                <label for="block" class="control-label">Block</label>
-                                <input type="number" id="block" name="block" value="<?= $l_block ?>" class="form-control">
-                            </div>
-                            <div class="col-md-2 form-group">
-                                <label for="lot" class="control-label">Lot</label>
-                                <input type="number" id="lot" name="lot" value="<?= $l_lot ?>" class="form-control">
-                            </div>
-                            <div class="col-md-3 form-group">
-                                <button class="btn btn-primary"><i class="dw dw-search"></i> Search Location</button>
-                                <!-- <button class="btn btn-default border btn-flat btn-sm" id="print" type="button"><i class="fa fa-print"></i> Print</button> -->
-                            </div>
-                        </div>
-                        </form>
+                    
                 </div>
             </div>
         </div>
