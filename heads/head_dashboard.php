8d2eb55c1416aa7ef3c743e415b2264c5fb9c3d1
galawangjudz <jaevoli@gmail.com>
2024-01-16 11:48:55 +0800

    add announcement

--- heads/head_dashboard.php
+++ heads/head_dashboard.php
@@ -20,6 +20,47 @@
 					</div>
 				</div>
 			</div>
+
+			<div class="card-box pd-20 height-1400-p mb-30">
+				<h1>🎉 Our new Utility Web App is live! 🖥️ </h1>
+				<p>
+					Starting this 1/15/2024, we encourage everyone to transition to the new system. 
+					Embrace efficiency and innovation! 💻✨</p>
+
+				<p>🕒 The Project Admin will initiate the Authority to Accept Payment (ATAP) using the new web app system. 
+					Cashiers, please issue Cash Acknowledgement Receipts (C.A.R.) manually in the meantime.</p>
+
+				<p>🖥️ All Cash Acknowledgement Receipts (C.A.R.) should be diligently encoded into the new system by Property Admin for record-keeping purposes. 
+					IT dept will help with the encoding of C.A.R if there is still a huge volume of clients. 
+					Just let us know for assistance.</p>
+
+				<p>Your cooperation in this process is crucial for a smooth transition. 
+					If you encounter any challenges, reach out to the IT department promptly.</p>
+
+				<p>Thank you for your commitment to this upgrade!</p>
+			</div>
+			<div class="card-box pd-20 height-1400-p mb-30">
+
+				<h1>🔐 Attention to all front liners and cashiers:</h1>
+				<p>Your account has been registered with the username as your employee ID, and the temporary password is also your employee ID.</p>
+				<p>For your security, please change your password immediately upon logging in.</p>
+
+				<p>Thank you for your prompt attention to this matter! If you encounter any issues, please reach out to the IT department. 🛡️🔒</p>
+
+			</div>
+
+			<div class="card-box pd-20 height-1400-p mb-30">
+				<h1>Reminders:</h1>
+						<ul>
+							<li>A 5% monthly surcharge for late payment.</li>
+							<li>A 2% rebate for payments made 3 days before the due date.</li>
+							<li>Previous billing until December 2023 will remain without surcharges.</li>
+						</ul>
+
+						<p class="font-18 mt-10">For inquiries, comments, or suggestions, please feel free to contact us at <a href="mailto:clientcare@asianland.ph">clientcare@asianland.ph</a>.</p>
+
+	
+			</div>
 			
 		</div>
 	</div>
